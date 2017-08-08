4ec3dac3899cb53ea86bc4a2946d71d5236fe2dd
Mason Preston <mpreston@onshift.com>
2017-08-08 07:50:09 -0400

    Add search_for_letters

--- heads_up_python/chapter_4/vsearch.py
+++ heads_up_python/chapter_4/vsearch.py
@@ -1,5 +1,11 @@
 
-def search_for_vowels(word: str) -> set:
+def search_for_vowels(phrase: str) -> set:
     """Return any vowels found in supplied word."""
     vowels = set('aeiou')
-    return vowels.intersection(set(word))
+    return vowels.intersection(set(phrase))
+
+
+def search_for_letters(phrase: str, letters: str='aeiou') -> set:
+    """Return a set of 'letters' found in 'phrase'"""
+    return set(letters).intersection(set(phrase))
+
