be836090d002480955b01feb6d62fc8a83a0fbac
Mason Preston <mpreston@onshift.com>
2017-08-21 15:01:03 -0400

    Add repr and default values

--- heads_up_python/chapter_6/countfromby.py
+++ heads_up_python/chapter_6/countfromby.py
@@ -1,7 +1,10 @@
 class CountFromBy:
-    def __init__(self, v: int, i: int) -> None:
+    def __init__(self, v: int=0, i: int=1) -> None:
         self.val = v
         self.incr = i
+
+    def __repr__(self) -> str:
+        return str(self.val)
 
     def increase(self) -> None:
         self.val += self.incr
