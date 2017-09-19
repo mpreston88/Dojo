d0af4da8fbf8f1cb79cb09ce94f791e4e68bcb42
Mason Preston <mpreston@onshift.com>
2017-09-18 20:10:26 -0400

    Add custom DB exceptions

--- heads_up_python/chapter_5/webapp/vsearch4web.py
+++ heads_up_python/chapter_5/webapp/vsearch4web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session
 from vsearch import search_for_letters
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError
 from checker import check_logged_in
 
 app = Flask(__name__)
@@ -67,16 +67,24 @@ def entry_page() -> 'html':
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """SELECT phrase, letters, ip, broswer_string, results
-                  FROM log;"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                           the_title='View Log',
-                           the_row_titles=titles,
-                           the_data=contents)
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """SELECT phrase, letters, ip, broswer_string, results
+                      FROM log;"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                               the_title='View Log',
+                               the_row_titles=titles,
+                               the_data=contents)
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-Id/Password issues. Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
 
 
 if __name__ == '__main__':
